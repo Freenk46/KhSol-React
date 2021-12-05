9c01d97f8fc883742e73f337877f4e6db4d9a010
Freenk46 <serguna.sergi@gmail.com>
2021-12-06 03:31:03 +0400

    First commit

--- src/components/Header/HeaderConteiner.jsx
+++ src/components/Header/HeaderConteiner.jsx
@@ -1,7 +1,5 @@
-import React from 'react';
 import Header from './Header'
 import {connect} from 'react-redux'
- 
   let mapStateToProps=(state) =>{
     return{
       HeaderNavbarDate: state.ManePagereducer.ManePage.HNbD
